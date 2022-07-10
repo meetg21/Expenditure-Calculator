49a3548db79afcb01ec339a2df4ad5a4f31bb7ba
meetg21 <94597802+meetg21@users.noreply.github.com>
2022-07-11 01:54:17 +0530

    added quit button

--- example.py
+++ example.py
@@ -46,8 +46,11 @@ def viewexpense():
     l=Label(root2,text=st,font=('arial',12),background="#CDBA96")
     l.place(y=100,x=20)
 
-    submitbtn=ttk.Button(root2,command=lambda: [root2.destroy(),home()],text="BACK",width=12)
-    submitbtn.grid(row=0,column=0,padx=16,pady=300)
+    style = ttk.Style()
+    style.configure("BW.TLabel",background = "#8B5A00",foreground = "white",font = ("arial",10,"bold","italic"))
+    backbtn=ttk.Button(root2,command=lambda: [root2.destroy(),home()],text="BACK",style="BW.TLabel",width=12)
+
+    backbtn.grid(row=0,column=0,padx=16,pady=300)
 
 
 
@@ -57,38 +60,41 @@ def home():
     global root
     root=Tk()
     root.title("Expense Tracker")
-    root.geometry('400x270')
+    root.geometry('1000x1000')
     root['background']='#CDBA96'
     root.resizable(False,False)
     pic = PhotoImage(file="stock.png")
     root.iconphoto(False,pic)
 
     dateLabel=Label(root,text="Date",font=('arial',15,'bold','italic'),bg="#8B5A00",fg="white",width=12)
-    dateLabel.grid(row=3,column=0,padx=7,pady=10)
+    dateLabel.grid(row=3,column=0,padx=3,pady=10)
 
-    dateEntry=DateEntry(root,width=12,font=('arial',15,'bold'),bg='#D5B77A')
-    dateEntry.grid(row=3,column=1,padx=7,pady=10)
+    x = ttk.Style()
+    x.configure("GW.TLabel",background = "#D5B77A",foreground = "black",font = ("arial",10,"bold","italic"))
+
+    dateEntry=DateEntry(root,width=80,x="GW.TLabel")
+    dateEntry.grid(row=3,column=1,padx=3,pady=10)
 
     Name=StringVar()
     nameLabel=Label(root, text="Name",font=('arial',15,'bold','italic'),bg="#8B5A00",fg="white",width=12)
-    nameLabel.grid(row=4,column=0,padx=7,pady=10)
+    nameLabel.grid(row=4,column=0,padx=3,pady=10)
 
-    NameEntry=Entry(root,textvariable=Name,font=('arial',15,'bold'),bg='#D5B77A')
-    NameEntry.grid(row=4,column=1,padx=7,pady=10)
+    NameEntry=Entry(root,width=60,textvariable=Name,font=('arial',15,'bold'),bg='#D5B77A')
+    NameEntry.grid(row=4,column=1,padx=3,pady=10)
 
     Title=StringVar()
     titleLabel=Label(root, text="Category",font=('arial',15,'bold','italic'),bg="#8B5A00",fg="white",width=12)
-    titleLabel.grid(row=5,column=0,padx=7,pady=10)
+    titleLabel.grid(row=5,column=0,padx=3,pady=10)
 
-    titleEntry=Entry(root,textvariable=Title,font=('arial',15,'bold'),bg='#D5B77A')
-    titleEntry.grid(row=5,column=1,padx=7,pady=10)
+    titleEntry=Entry(root,width=60,textvariable=Title,font=('arial',15,'bold'),bg='#D5B77A')
+    titleEntry.grid(row=5,column=1,padx=3,pady=10)
 
     Expense=IntVar()
     expenseLabel=Label(root,text="Expense",font=('arial',15,'bold','italic'),bg="#8B5A00",fg="white",width=12)
-    expenseLabel.grid(row=6,column=0,padx=7,pady=10)
+    expenseLabel.grid(row=6,column=0,padx=3,pady=10)
 
-    expenseEntry=Entry(root,textvariable=Expense,font=('arial',15,'bold'),bg='#D5B77A')
-    expenseEntry.grid(row=6,column=1,padx=7,pady=10)
+    expenseEntry=Entry(root,width=60,textvariable=Expense,font=('arial',15,'bold'),bg='#D5B77A')
+    expenseEntry.grid(row=6,column=1,padx=3,pady=10)
 
     def submitexpense():
         values=[dateEntry.get(),Name.get(),Title.get(),Expense.get()]
@@ -103,14 +109,17 @@ def home():
             viewexpense()
         else:
             messagebox.showerror("Error","Invalid value")
+    
+    style = ttk.Style()
+    style.configure("BW.TLabel",background = "#8B5A00",foreground = "white",font = ("arial",10,"bold","italic"))
+    submitbtn=ttk.Button(root,command=submitexpense,text="Submit",style="BW.TLabel",width=8)
+    submitbtn.grid(row=7,column=0,padx=3,pady=5)
+    
+    expensebtn=ttk.Button(root,command=viewexpense,text="Expenses",style="BW.TLabel",width=9 )
+    expensebtn.grid(row=7,column=1,padx=7,pady=5)
 
-    submitbtn=ttk.Button(root,command=submitexpense,text="Submit",width=12)
-    submitbtn.grid(row=7,column=0,padx=13,pady=16)
-    # submitbtn.configure(bg="#8B5A00",fg="white")
-
-
-    submitbtn=ttk.Button(root,command=viewexpense,text="Expenses",width=12 )
-    submitbtn.grid(row=7,column=1,padx=13,pady=16)
+    quitBtn = ttk.Button(root,text="Quit",style="BW.TLabel", command=root.destroy,width=5)
+    quitBtn.grid(row=7,column=2,padx=10,pady=5)
     mainloop()
 
 home()
